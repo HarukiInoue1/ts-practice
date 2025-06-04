b149d88464164f342352dc691a59e7b943f030ad
HarukiInoue1 <inoueh@tanapism.co.jp>
2025-06-04 20:59:23 +0900

    非同期処理

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,91 @@
-const customers = [
-  { name: "山本", id: 101 },
-  { name: "高橋", id: 102 },
-  { name: "伊藤", id: 103 },
-]
+// // 同期
+// console.log("A: 注文する")
 
-customers.forEach((customer) => console.log(`${customer.name}さん`))
+// console.log("B: ラーメン完成！")
+
+// console.log("C: 水を飲む") //→ラーメン完成するまでに水を飲みたい、でも同期処理だとラーメン完成まで待たないといけない
+
+// // 非同期
+// console.log("A: 注文する")
+
+// setTimeout(() => {
+//   console.log("B: ラーメン完成！")
+// }, 2000)
+
+// console.log("C: 水を飲む")
+
+// Promiseオブジェクト作成 → 非同期の結果をラップする箱
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("エラー") //resolve または reject は どちらか一度だけしか効かない →Promiseはpendingから状態が変わったら確定(settled)されるから
+    resolve("成功")
+  }, 1000)
+})
+
+// console.log(promise) //  Promiseの状態 <pending> 1秒間
+
+// // A: Promiseのthen/catch
+
+// // Resolveされた結果　1秒後 resolveでfulfilledになったらthenチェーン　失敗になったらcatchでエラー表示　resolveの結果関係なく処理の最終結果
+// promise
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error))
+//   .finally(() => console.log("処理終了"))
+
+// B: async/awaitで同じことをやる
+// async関数は、常にPromiseを返す関数　→　Promiseはthenチェーンで繋いでいくと長く読みにくくなるから
+async function asyncExample() {
+  try {
+    const result = await promise //awaitはPromiseがresolveされるのを待つ構文
+    console.log(result) //エラーが出たら実行されない
+  } catch (err) {
+    console.log(err) //rejectされた場合はcatchにいく
+  } finally {
+    console.log("終了")
+  }
+}
+
+asyncExample()
+
+// async function makeRamen() {
+//   console.log("A: 注文する")
+
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("B: ラーメン完成！")
+//       // reject("B: ラーメン失敗")
+//     }, 1000)
+//   })
+
+//   console.log("C: 水を飲む")
+
+//   try {
+//     const result = await promise
+//     console.log(result)
+//   } catch (err) {
+//     console.log("エラー:", err)
+//   } finally {
+//     console.log("D: 食事終了")
+//   }
+// }
+
+// makeRamen()
+
+// function axiosGet(url: string): Promise<any> {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open("GET", url)
+
+//     xhr.onload = () => {
+//       if (xhr.status === 200) {
+//         resolve(xhr.responseText) // ✅ 通信成功したら resolve！
+//       } else {
+//         reject(new Error("通信エラー")) // ❌ ステータスコードがエラーなら reject！
+//       }
+//     }
+
+//     xhr.onerror = () => reject(new Error("ネットワークエラー"))
+
+//     xhr.send()
+//   })
+// }
